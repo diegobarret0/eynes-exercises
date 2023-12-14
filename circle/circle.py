3f208b23e3a59fe9037a913d544d610032e68cc9
diegobarret0 <114023879+diegobarret0@users.noreply.github.com>
2023-12-14 19:22:11 -0300

    Update circle.py
    Resolucion de ejercicio circle.py

--- circle/circle.py
+++ circle/circle.py
@@ -1,4 +1,33 @@
 import math
 
-class Circle():
-    pass
+class Circle:
+    def __init__(self, radius):
+        if (radius <= 0):
+            raise ValueError("No se pudo crear el circulo, el radio debe ser mayor a 0")
+        self.radius = radius
+
+    def get_radius(self):
+        return self.radius
+    
+    def set_radius(self, radius):
+        if (radius <= 0):
+            raise ValueError("El radio debe ser mayor a 0")
+        self.radius = radius
+
+    def get_area(self):
+        return (math.pi * (self.radius ** 2))
+    
+    def get_perimeter(self):
+        return (2 * math.pi * self.radius)
+    
+    def __mul__(self, n):
+        if (n <= 0):
+            raise ValueError("El numero debe ser mayor a 0")
+        return Circle(self.radius * n)
+    
+    def __str__(self):
+        return "Radio: {}\nArea: {}\nPerimetro:{}".format(
+            self.radius, 
+            self.get_area(), 
+            self.get_perimeter()
+            )
