cb04147de642105aa32b1be197c41f276c6f1a12
diegobarret0 <114023879+diegobarret0@users.noreply.github.com>
2023-12-14 19:26:12 -0300

    Update transpose.py
    Resolucion de transpose.py

--- matrix/transpose.py
+++ matrix/transpose.py
@@ -1,2 +1,11 @@
 def transpose(matrix):
-    pass
+
+    transpose_matrix = []
+
+    for i in range(len(matrix[0])):
+        transpose_matrix.append([row[i] for row in matrix])
+    
+    return transpose_matrix
+
+matrix = [[5, 6, 3],[2 ,3 ,4]]
+print(transpose(matrix))
